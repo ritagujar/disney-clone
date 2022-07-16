ff8bb6469ff85da5927c45d6221020db1b42863a
759153 <ritagujar14@gmail.com>
2022-07-16 22:24:39 +0530

    Added Sign in with Google Popup

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCvQwqBoLtcUKBIfgXqexooQSuNon0m3eg",
@@ -11,7 +14,7 @@ const firebaseConfig = {
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.fireStore();
+const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 const storage = firebase.storage();
