27ee99ee12480cd3d161e9d125e60ab06cf47534
759153 <ritagujar14@gmail.com>
2022-07-16 19:58:20 +0530

    navMenu setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ const Header = (props) => {
       <Logo>
         <img src="/images/logo.svg" alt="Disney" />
       </Logo>
+      <NavMenu>menu</NavMenu>
     </Nav>
   );
 };
@@ -37,6 +38,23 @@ const Logo = styled.a`
   img {
     display: block;
     width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row wrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0px;
+  padding: 0px;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+
+  @media (max-width: 768px) {
+    display: none;
   }
 `;
 
